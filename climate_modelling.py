159e3823a6a71b405d90b76b9d3f704728d5c8c5
ayushdpsg12 <72147048+ayushdpsg12@users.noreply.github.com>
2023-11-02 00:02:34 +0530

    climate_modelling.py

--- climate_modelling.py
+++ climate_modelling.py
@@ -5,13 +5,13 @@ from statsmodels.tsa.statespace.sarimax import SARIMAX
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from sklearn.metrics import mean_squared_error
 
-# Load real climate data (replace with your dataset)
-data_url = 'path_to_real_climate_data.csv'
+# Load climate data
+data_url = 'climate_data.csv'
 df = pd.read_csv(data_url)
 df['Date'] = pd.to_datetime(df['Date'])
 df.set_index('Date', inplace=True)
 
-# Visualize the time series data
+# Visualizing the time series data
 plt.figure(figsize=(10, 6))
 plt.plot(df.index, df['Temperature'], label='Observed Temperature')
 plt.xlabel('Date')
@@ -20,7 +20,7 @@ plt.title('Observed Temperature Time Series')
 plt.legend()
 plt.show()
 
-# Split data into training and testing sets
+# Spliting data into training and testing sets
 train_size = int(0.8 * len(df))
 train_data = df.iloc[:train_size]
 test_data = df.iloc[train_size:]
@@ -29,7 +29,7 @@ test_data = df.iloc[train_size:]
 order = (1, 1, 1)
 seasonal_order = (1, 1, 1, 12)
 
-# Fit a SARIMA model with additional features
+# Fitting SARIMA model with additional features
 exog_train = train_data[['Greenhouse_Gases', 'Ocean_Surface_Temperature']]
 exog_test = test_data[['Greenhouse_Gases', 'Ocean_Surface_Temperature']]
 model = SARIMAX(
@@ -39,16 +39,16 @@ model = SARIMAX(
 )
 fitted_model = model.fit(disp=False)
 
-# Predict using the fitted model
+# Predicting using the fitted model
 predictions = fitted_model.get_forecast(steps=len(test_data), exog=exog_test)
 predicted_mean = predictions.predicted_mean
 conf_int = predictions.conf_int()
 
-# Calculate Mean Squared Error (MSE) for evaluation
+# Calculating Mean Squared Error (MSE) for evaluation
 mse = mean_squared_error(test_data['Temperature'], predicted_mean)
 print(f"Mean Squared Error: {mse:.2f}")
 
-# Visualize the predictions and confidence interval
+# Visualizing the predictions and confidence interval
 plt.figure(figsize=(10, 6))
 plt.plot(train_data.index, train_data['Temperature'], label='Train Temperature')
 plt.plot(test_data.index, test_data['Temperature'], label='Test Temperature')
@@ -60,13 +60,13 @@ plt.title('Temperature Time Series Prediction with SARIMA and Additional Feature
 plt.legend()
 plt.show()
 
-# Plot ACF and PACF to help with parameter selection
+# Plotting ACF and PACF to help with parameter selection
 fig, axes = plt.subplots(1, 2, figsize=(14, 4))
 plot_acf(train_data['Temperature'], lags=40, ax=axes[0])
 plot_pacf(train_data['Temperature'], lags=40, ax=axes[1])
 plt.show()
 
-# Print model summary and residuals analysis
+# Printing model summary and residuals analysis
 print(fitted_model.summary())
 plt.figure(figsize=(10, 6))
 plt.plot(fitted_model.resid)
